e133b044274c6fa1df6284fddccc9ff02831a8ba
Mark Kazanski <markkazanski@gmail.com>
2018-01-13 17:06:32 -0600

    added env vars

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,14 @@
 // *********************************************************************************
 // CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
 // *********************************************************************************
+require('dotenv').config({path: __dirname + '/.env'});
 
 // Dependencies
 var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize
-var sequelize = new Sequelize("burgers_db", "root", "Arseface04!", {
-  host: "localhost",
+var sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
   dialect: "mysql",
   pool: {
     max: 5,
