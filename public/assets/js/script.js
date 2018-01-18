fea0531771efaca5e703f8347d51857c55928284
Alize Nguyen <alizenguyen@gmail.com>
2018-01-17 19:49:09 -0600

    modal added

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,17 +1,7 @@
-$(document).ready(function() {
-    
-    $(".devour-form").on("submit", function(event) {
-    event.preventDefault();
-
-    var burger_id = $(this).children(".burger_id").val();
-    console.log(burger_id)
-    $.ajax({
-        method: "PUT",
-        url: "/burgers/" + burger_id
-    }).then(function(data) {
-        // reload page to display devoured burger in proper column
-        location.reload();
-    })
-
-    })
+$(document).ready(function () {
+    $(".innersignuptext").click(function () {
+        $('#signup-modal').modal('show');
+        $("#signup-modal").css({
+            opacity: 0.95
+        });
 });
