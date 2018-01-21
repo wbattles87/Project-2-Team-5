f90836d64cf6b3ec1d7365bcf9d4ca58bdb474a0
Mark Kazanski <markkazanski@gmail.com>
2018-01-20 18:23:42 -0600

    request working

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -3,7 +3,6 @@ const db = require("../models");
 module.exports = function(app) {
 
     app.get("/", function(req, res){
-        console.log(res);
         //res.sendFile(__dirname + "../public/home-test.html"); 
         res.render("index", { recipe_data: { id:"1", recipe_name: "TEST RECIPE" } });
         //something like this. Need to convert home-test to use handlebars
--- controllers/recipe-api-routes.js
+++ controllers/recipe-api-routes.js
@@ -1,4 +1,5 @@
 const db = require("../models");
+var request = require('request');
 
 module.exports = function(app) {
 
@@ -10,7 +11,20 @@ module.exports = function(app) {
     app.post("/api/recipe", function(req, res){
         //add recipe
         //db.Recipe.create(...)
-        //req.body.recipe_url
+        //var newUrl = req.body.recipe_url
+        var newUrl = "http://allrecipes.com/recipe/234610/cinnamon-oatmeal-bars/";
+        var newHTML;
+
+        request(newUrl, function(error, response, body) {
+
+            // If the request was successful...
+            if (!error && response.statusCode === 200) {
+          
+              // Then log the body from the site!
+              console.log(body);
+              newHTML = body;
+            }
+          });
     });
 
 };
