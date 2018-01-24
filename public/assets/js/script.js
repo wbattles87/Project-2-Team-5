fd413518584e87f1cff4109273c81cd0971299f6
Alize Nguyen <alizenguyen@gmail.com>
2018-01-24 00:04:35 -0600

    delete and edit button setup

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,10 +1,35 @@
 $(document).ready(function () {
+    //SIGN UP MODAL JAVASCRIPT
     $(".innersignuptext").click(function () {
         $('#signup-modal').trigger('click');
         $("#signup-modal").css({ opacity: 0.95 });
     });
 
-    /*TEMPORARY Burgers thing*/
+    //Recipe -> Ingredient Button (convert to ingredient page when clicked)
+    $(".recipeName").on("click", function() {
+        $ajax({
+            url:`/burgers/eat/${$(this).attr("data-id")}`,
+            method: "GET"
+        }).then(function(data){
+            console.log("Then data: " + data);
+            location.reload();
+        })
+    });
+
+    //Delete Ajax (DELETE) Calls
+    $(".recipeDelete").on("click", function() {
+        $ajax({
+            url:`/burgers/eat/${$(this).attr("data-id")}`,
+            method: "DELETE"
+        }).then(function(data){
+            console.log("Then data: " + data);
+            location.reload();
+        })
+    });
+
+    //Delete Ajax (PUT) Calls
+
+    /*TEMPORARY Burgers thing - TEMPLATE*/
     $(".eatit").on("click", function(){
         $.ajax({
             url:`/burgers/eat/${$(this).attr("data-id")}`,
