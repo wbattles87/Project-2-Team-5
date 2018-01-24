5fe58586dce2b94169d367154b54bae279546713
Adeel Ahmed <adeelahmed@Adeels-MacBook-Pro.local>
2018-01-23 21:38:06 -0600

    working on display routes

--- controllers/recipe-display-routes.js
+++ controllers/recipe-display-routes.js
@@ -5,25 +5,51 @@ module.exports = function(app) {
     app.get("/recipe", function(req, res){
         //check that logged in. pass user_id to callback
         db.Recipe.findAll({}).then(function(result) {
-            res.render("recipe", { recipe_data: result }); //goes to recipes.handlebars
+            res.render("recipes", { recipe_data: result }); //goes to recipes.handlebars
         });
+      });
 
 
-    app.get("/ingredient", function(req, res){
-        db.Recipe.findAll({
+    app.get("/ingredient/:recipeId", function(req, res){
+        db.Recipe.findOne({
         	where: {
-        		id: Recipe.id
+        		id: req.params.recipeId
         	},
-        	include: [db.ingredient]
-        }).then
+        	include: [db.Ingredient]
 
         }).then(function(result) {
-            res.render("recipe", { recipe_data: result }); //goes to recipes.handlebars
+          console.log(result)
+            //res.render("recipe", { recipe_data: result }); //goes to recipes.handlebars
+            res.json(result);
         });
+
     });
 
+    app.get("/instruction/:recipeId", function(req, res){
+        db.Recipe.findOne({
+          where: {
+            id: req.params.recipeId
+          },
+          include: [db.Instruction]
+
+        }).then(function(result) {
+          console.log(result)
+            //res.render("recipe", { recipe_data: result }); //goes to recipes.handlebars
+            res.json(result);
+        });
+
+    });
 };
 
+/*
+db.Ingredients.findAll({
+  where:{
+    recipeId: req.params.id
+  }
+}).then(function(results){
+  console.log()
+})
+*/
 
 /*
 module.exports = function(app) {
