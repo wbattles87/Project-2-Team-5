c1b9e6f8d7ebbe0fe44d485dabc0cb1bec32b6f1
Adeel Ahmed <adeelahmed@Adeels-MacBook-Pro.local>
2018-01-23 20:47:28 -0600

    syncing fork with master

--- controllers/recipe-display-routes.js
+++ controllers/recipe-display-routes.js
@@ -2,11 +2,53 @@ const db = require("../models");
 
 module.exports = function(app) {
 
-    app.get("/recipes", function(req, res){
+    app.get("/recipe", function(req, res){
         //check that logged in. pass user_id to callback
         db.Recipe.findAll({}).then(function(result) {
+            res.render("recipe", { recipe_data: result }); //goes to recipes.handlebars
+        });
+
+
+    app.get("/ingredient", function(req, res){
+        db.Recipe.findAll({
+        	where: {
+        		id: Recipe.id
+        	},
+        	include: [db.ingredient]
+        }).then
+
+        }).then(function(result) {
             res.render("recipe", { recipe_data: result }); //goes to recipes.handlebars
         });
     });
 
 };
+
+
+/*
+module.exports = function(app) {
+  // HTML GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases the user is shown an HTML page of content
+  // ---------------------------------------------------------------------------
+
+  app.get("/recipe", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/recipe.html"));
+  });
+
+  app.get("/ingredient", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/ingredient.html"));
+  });
+
+	app.get("/instruction", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/instruction.html"));
+  });
+
+  // If no matching route is found default to home
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/reserve.html"));
+  });
+};
+
+
+*/
