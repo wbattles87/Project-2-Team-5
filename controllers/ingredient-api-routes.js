99af7bd0f3d513591b07eaad882834dbe99e0622
Alize Nguyen <alizenguyen@gmail.com>
2018-01-24 17:56:57 -0600

    ingredient api routes added

--- controllers/ingredient-api-routes.js
+++ controllers/ingredient-api-routes.js
@@ -1,13 +1,25 @@
 const db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-    app.get("/ingredients/api", function(req, res){
+    app.get("/api/ingredients/:id", function (req, res) {
         //get ingredients
+        db.Ingredient.findAll({
+            where: {
+                RecipeId: req.params.id
+            }
+        }).then(function (data) {
+            console.log(data);
+            res.json(data);
+        });
     });
 
-    app.post("/ingredients/api", function(req, res){
+    app.post("/api/ingredients", function (req, res) {
         //add ingredients
+        console.log(req.body);
+        db.Ingredient.create(req.body).then(function (Ingredient) {
+            res.json(Ingredient);
+        });
     });
 
 };
