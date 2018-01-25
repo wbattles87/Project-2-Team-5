7ce134ba45c7beb2f75680204cda5521b504cb8b
Alize Nguyen <alizenguyen@gmail.com>
2018-01-24 18:38:41 -0600

    Delete function added

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -3,15 +3,17 @@ $(document).ready(function () {
     //SIGN UP MODAL JAVASCRIPT
     $(".innersignuptext").click(function () {
         $('#signup-modal').trigger('click');
-        $("#signup-modal").css({ opacity: 0.95 });
+        $("#signup-modal").css({
+            opacity: 0.95
+        });
     });
 
     //NavBar Logo to return to Homepage
-    $("#navbarlogo").on("click", function() {
+    $("#navbarlogo").on("click", function () {
         $.ajax({
-            url:`/`,
+            url: `/`,
             method: "GET",
-        }).then(function(data){
+        }).then(function (data) {
             console.log(data);
             console.log("Returning to Home");
             $(".body-template").html(data); //need better way - should load original page/path
@@ -19,19 +21,19 @@ $(document).ready(function () {
     });
 
     //Recipe Search Function
-    $(".search-button").on("click", function() {
+    $(".search-button").on("click", function () {
         event.preventDefault();
 
         var searchQuery = $("#userSearch").val().trim().split(' ');
         console.log(searchQuery);
 
         $.ajax({
-            url:`RECIPE NAMES`,
+            url: `RECIPE NAMES`,
             method: "GET"
-        }).then(function(data){
+        }).then(function (data) {
             console.log("Then data: " + data);
 
-            for (i=0; i<searchQuery.length; i+=1) {
+            for (i = 0; i < searchQuery.length; i += 1) {
                 //break recipe name into array - two for loops?
                 //need to compare searchQuery array against each recipe name array
             };
@@ -39,37 +41,38 @@ $(document).ready(function () {
     });
 
     //Ingredient To-Do List
-    $(".ingredientChecklist").on("click", function(ev) {
+    $(".ingredientChecklist").on("click", function (ev) {
         ev.target.parentElement.classList.toggle("checked");
     });
 
     //Recipe -> Ingredient Button (convert to ingredient page when clicked)
-    $(".recipeName").on("click", function() {
+    $(".recipeName").on("click", function () {
         $.ajax({
-            url:`/burgers/eat/${$(this).attr("data-id")}`,
+            url: `/burgers/eat/${$(this).attr("data-id")}`,
             method: "GET"
-        }).then(function(data){
+        }).then(function (data) {
             console.log("Then data: " + data);
             location.reload();
         })
     });
 
     //Delete Ajax (DELETE) Calls
-    $(".recipeDelete").on("click", function() {
+    $(".recipeDelete").on("click", function () {
+        var listItemData = $(this).parent("td").parent("tr").data("RecipeId");
+        var id = listItemData.id;
         $.ajax({
-            url:`/api/recipes/${$(this).attr("RecipeId")}`,
-            method: "DELETE"
-        }).then(function(data){
-            console.log("Then data: " + data);
-        })
+                method: "DELETE",
+                url: "/api/authors/" + id
+            })
+            .then(getAuthors);
     });
 
     //Edit Ajax (PUT) Calls
-    $(".recipeEdit").on("click", function() {
+    $(".recipeEdit").on("click", function () {
         $.ajax({
-            url:`/burgers/eat/${$(this).attr("data-id")}`,
+            url: `/burgers/eat/${$(this).attr("data-id")}`,
             method: "PUT"
-        }).then(function(data){
+        }).then(function (data) {
             console.log("Then data: " + data);
             location.reload();
         })
