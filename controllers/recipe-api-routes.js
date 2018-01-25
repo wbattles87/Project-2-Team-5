beffab686589f370f35ac16da5136b00c9c3e5de
Alize Nguyen <alizenguyen@gmail.com>
2018-01-25 00:53:40 -0600

    complete put function working

--- controllers/recipe-api-routes.js
+++ controllers/recipe-api-routes.js
@@ -21,19 +21,18 @@ module.exports = function (app) {
         });
     });
 
-    app.put("/api/recipes/update/:id", function(req, res) {
-        var condition = "id = " + req.params.id;
-      
-        console.log("condition", condition);
-      
-        db.Recipe.updateOne({recipe_checkbox: true}, condition, function(result) {
-          if (result.changedRows == 0) {
-            // If no rows were changed, then the ID must not exist, so 404
-            return res.status(404).end();
-          } else {
-            res.status(200).end();
-          }
-      });
+    app.put("/api/recipes/update/:id", function (req, res) {
+        db.Recipe.findOne({
+            where: {
+                id: req.params.id
+            },
+        }).then(function (data) {
+            return data.update({
+                recipe_checkbox: true
+            });
+        }).then(function (record) {
+            res.sendStatus(200);
+        });
     });
 
     app.post("/api/recipe", function (req, res) {
