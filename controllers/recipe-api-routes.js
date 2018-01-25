7e909625bdf4cf2b7c9a274c8b442367a37adace
Mark Kazanski <markkazanski@gmail.com>
2018-01-25 00:50:44 -0600

    json obj parsing

--- controllers/recipe-api-routes.js
+++ controllers/recipe-api-routes.js
@@ -77,11 +77,20 @@ module.exports = function (app) {
                         })
                         .then(function (responseRecipe) {
                             var recipeId = responseRecipe.dataValues.id; //user reicpe id of ingr and instr
-                            db.Ingredient.bulkCreate(parseItempropIngredients($, recipeId), {
+                            
+                            var ingredsArrayTemp = parseItempropIngredients($, recipeId);
+                            if(!ingredsArrayTemp.length){ //check if no ingreds
+                                ingredsArrayTemp[0] = { ingredient_info:"No Ingredients Detected", RecipeId: recipeId };
+                            }
+                            db.Ingredient.bulkCreate( ingredsArrayTemp , {
                                     individualHooks: true
                                 })
                                 .then(function (responseIngredient) {
-                                    db.Instruction.bulkCreate(parseItempropInstructions($, recipeId), {
+                                    var instructionsArrayTemp = parseItempropInstructions($, recipeId);
+                                    if(!instructionsArrayTemp.length){ //check if no instructions
+                                        instructionsArrayTemp[0] = { instruction_info:"No Instructions Detected", RecipeId: recipeId };
+                                    }
+                                    db.Instruction.bulkCreate(instructionsArrayTemp, {
                                             individualHooks: true
                                         })
                                         .then(function (responseInstruction) {
@@ -125,28 +134,83 @@ module.exports = function (app) {
 
 function parseItempropIngredients($, recipeId) {
     var ingredientsArray = [];
-    $('[itemprop]').map(function (i, el) { //get list of elements with itemprop attr
-        // this === el 
-        if ($(this).attr("itemprop").match(/ngredient/)) //all itemprops that match I/ingredient
-            ingredientsArray.push({
-                ingredient_info: $(this).text(),
-                RecipeId: recipeId
-            }); //pushes an object
+    var jsonFound = false;
+
+    //Look for JSON object in page
+    $("script").each(function(){ 
+        if( $(this).attr("type") === "application/ld+json" ){
+          if( JSON.parse( $(this).html() )["@type"] === "Recipe" ){
+            if( JSON.parse( $(this).html() )["recipeIngredient"] ){
+                ingredientsArray = JSON.parse( $(this).html() )["recipeIngredient"];
+                jsonFound = true;
+                console.log(ingredientsArray);    
+            }else 
+              console.log("No Ingredients");
+          }else 
+            console.log("No Recipe in JSON OBJ");
+        }else  
+            console.log("No JSON Obj");
     });
+
+    if(jsonFound && ingredientsArray.length){ //push objects with RecipeID
+        for(let i=0; i<ingredientsArray.length; i++){
+            ingredientsArray[i] = { ingredient_info: ingredientsArray[i], RecipeId: recipeId };
+        }
+    }
+
+    if(!ingredientsArray.length){ //check that no JSON obj in page then continue parsing
+    //$('[itemprop="ingredients"]')
+        $('[itemprop]').map(function (i, el) { //get list of elements with itemprop attr
+            // this === el 
+            if ($(this).attr("itemprop").match(/ngredient/)) //all itemprops that match I/ingredient
+                ingredientsArray.push({
+                    ingredient_info: $(this).text(),
+                    RecipeId: recipeId
+                }); //pushes an object
+        });
+    }
+
+
     return ingredientsArray;
 }
 
 function parseItempropInstructions($, recipeId) {
     var instructionsArray = [];
     var instructionArrayClean = [];
+    var jsonFound = false;
 
-    $('[itemprop]').map(function (i, el) { //get list of elements with itemprop attr
-        // this === el
-        if ($(this).attr("itemprop").match(/nstructions/)) { //all itemprops that match I/instructions
-            console.log("instructionsArray: " + i + " : " + el + " : " + $(this).text().split('\n'));
-            instructionsArray.push($(this).text().split('\n')); //split items by line breaks
-        }
+    //Look for JSON object in page
+    $("script").each(function(){ 
+        if( $(this).attr("type") === "application/ld+json" ){
+          if( JSON.parse( $(this).html() )["@type"] === "Recipe" ){
+            if( JSON.parse( $(this).html() )["recipeInstructions"] ){
+                instructionArrayClean = JSON.parse( $(this).html() )["recipeInstructions"].split(". ");
+                jsonFound = true;
+                console.log(instructionArrayClean);
+            }
+            else 
+              console.log("No Instructions");
+          }else 
+            console.log("No Recipe in JSON OBJ");
+        }else  
+            console.log("No JSON Obj");
     });
+
+    if(jsonFound && instructionArrayClean.length){ //push objects with RecipeID
+        for(let i=0; i<instructionArrayClean.length; i++){
+            instructionArrayClean[i] = { instruction_info: (i+1) + ". " + instructionArrayClean[i] + ".", RecipeId: recipeId };
+        }
+    }
+
+    if(!instructionArrayClean.length){ //if no JSON obj in page then continue parsing
+        $('[itemprop]').map(function (i, el) { //get list of elements with itemprop attr
+            // this === el
+            if ($(this).attr("itemprop").match(/nstructions/)) { //all itemprops that match I/instructions
+                console.log("instructionsArray: " + i + " : " + el + " : " + $(this).text().split('\n'));
+                instructionsArray.push($(this).text().split('\n')); //split items by line breaks
+            }
+        });
+    }
 
     var counter = 0;
     for (let i = 0; i < instructionsArray.length; i++) { //Run through array of arrays and put all instructions in order
