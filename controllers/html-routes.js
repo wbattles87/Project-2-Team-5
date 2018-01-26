2d401910fdafb11f5ca5195096d8d679db6ded58
Mark Kazanski <markkazanski@gmail.com>
2018-01-25 20:29:45 -0600

    route

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -4,8 +4,8 @@ module.exports = function(app) {
 
     app.get("/", function(req, res){
         //res.sendFile(__dirname + "../public/home-test.html"); 
-        //res.render("index");
-        res.send("HELLO");
+        res.render("index");
+        //res.send("HELLO");
         //console.log("Random text");
         //something like this. Need to convert home-test to use handlebars
     });
