b29f92225456bf1b8bf63090ec44c5ab8803fa27
Mark Kazanski <markkazanski@gmail.com>
2018-01-25 20:01:38 -0600

    home route fix

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -5,6 +5,7 @@ module.exports = function(app) {
     app.get("/", function(req, res){
         //res.sendFile(__dirname + "../public/home-test.html"); 
         res.render("index");
+        console.log("Random text");
         //something like this. Need to convert home-test to use handlebars
     });
     //login page
