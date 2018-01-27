f12e2b832266c550fbf87f4317cd75ca2445e143
Mark Kazanski <markkazanski@gmail.com>
2018-01-26 22:46:33 -0600

    bookmarkelt local

--- public/bookmarklet.js
+++ public/bookmarklet.js
@@ -42,9 +42,9 @@ function sendBookmark(){
     });
   */
 
-  //GET //http://fierce-island-55952.herokuapp.com/api/bookmarklet
+  //GET //
   jQuery.ajax({
-    url: `http://localhost:8000/api/bookmarklet?recipe_url=${newURL}`,
+    url: `http://fierce-island-55952.herokuapp.com/api/bookmarklet?recipe_url=${newURL}`,
     method: "GET",
     crossDomain: true
   }).done(function(response) {
